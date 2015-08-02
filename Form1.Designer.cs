488256ab7bf9bd2f9851aad812796dd59d31f8f7
xhn11 <xhn11@centrum.cz>
2015-08-02 02:14:13 +0200

    add
    add

---
+++ Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace photoshop_panel
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.button3 = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(110, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Get Layers List";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(197, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(13, 78);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(259, 23);
+            this.progressBar1.TabIndex = 2;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(13, 43);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(110, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(284, 24);
+            this.menuStrip1.TabIndex = 4;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(13, 107);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(259, 232);
+            this.flowLayoutPanel1.TabIndex = 5;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(197, 41);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 351);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.Xml;
+using ps = Photoshop;
+
+
+namespace photoshop_panel
+{
+    public partial class Form1 : Form
+    {
+
+        //global var
+        string[] names;
+        string[] export_ui;
+        //
+        
+        //Start Photoshop Functions
+
+        Photoshop.Application app = new Photoshop.Application();
+
+        private void Isolate_Selection()
+        {
+            var idShw = app.CharIDToTypeID("Shw ");
+            var desc68 = new ps.ActionDescriptor();
+            var idnull = app.CharIDToTypeID("null");
+            var list21 = new ps.ActionList();
+            var ref49 = new Photoshop.ActionReference();
+            var idLyr = app.CharIDToTypeID("Lyr ");
+            var idOrdn = app.CharIDToTypeID("Ordn");
+            var idTrgt = app.CharIDToTypeID("Trgt");
+            ref49.PutEnumerated(idLyr, idOrdn, idTrgt);
+            list21.PutReference(ref49);
+            desc68.PutList(idnull, list21);
+            var idTglO = app.CharIDToTypeID("TglO");
+            desc68.PutBoolean(idTglO, true);
+            app.ExecuteAction(idShw, desc68, ps.PsDialogModes.psDisplayNoDialogs);
+        }
+
+        private void Copy_merge()
+        {
+            var idCpyM = app.CharIDToTypeID("CpyM");
+            app.ExecuteAction(idCpyM, null, ps.PsDialogModes.psDisplayNoDialogs);
+        }
+
+        private void Copy()
+        {
+            var idCpyM = app.CharIDToTypeID("Copy");
+            app.ExecuteAction(idCpyM, null, ps.PsDialogModes.psDisplayNoDialogs);
+        }
+
+        private int Get_Layers_Count()
+        {
+            Photoshop.ActionReference act = new Photoshop.ActionReference();
+            act.PutEnumerated(app.CharIDToTypeID("Dcmn"), app.CharIDToTypeID("Ordn"), app.CharIDToTypeID("Trgt"));
+            var desc = app.ExecuteActionGet(act);
+            return desc.GetInteger(app.CharIDToTypeID("NmbL"));
+        }
+
+        private string Get_Active_History()
+        {
+            return app.ActiveDocument.ActiveHistoryState.Name;
+        }
+
+        private string Get_Layer_Name(int index)
+        {
+            Photoshop.ActionReference act = new Photoshop.ActionReference();
+            act.PutIndex(app.CharIDToTypeID("Lyr "), index+1);
+            return app.ExecuteActionGet(act).GetString(app.CharIDToTypeID("Nm  "));
+        }
+
+        private void Refresh_Layers_list()
+        {
+            int layers_count = Get_Layers_Count();
+
+            names = new string[Get_Layers_Count()];
+
+            progressBar1.Maximum = Get_Layers_Count()-1;
+            progressBar1.Step = 1;
+            progressBar1.Value = 0;
+
+            for (int i = 0; i < layers_count - 1; i++)
+            {
+                names[i] = Get_Layer_Name(i);
+                progressBar1.PerformStep();
+            };
+        }
+
+        private void Set_for_Export(string name, string target)
+        {
+
+        }
+        //End Photoshop Functions
+
+        public int Index_By_Name(string name)
+        {
+            for (int i = 0; i < names.Length +1; i++)
+            {
+                if (names[i] == name)
+                {
+                    return i;
+                }
+            }
+            return 0;
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+   
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Refresh_Layers_list();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            TopMost = true;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            XmlReader reader = XmlReader.Create("UltimateTextureSaver.xml");
+
+            reader.Read();
+
+            string text = "";
+
+            while (reader.Read())
+            {
+                int i = 1;
+                //MessageBox.Show(reader);
+                if(reader.HasAttributes)
+                {
+                    if (reader.Name == "Texture")
+                    {
+                        reader.MoveToAttribute(0);
+                        Button button_export = new Button();
+
+                        button_export.Name = "button_export" + reader.Name;
+                        button_export.Text = reader.Value;
+                        flowLayoutPanel1.Controls.Add(button_export);
+
+                    }
+
+
+                    //reader.MoveToAttribute(0);
+                    // Do some work here on the data.
+                    text = text + "<" + reader.Name + "//" + reader.NodeType +"--"+ reader.Value + ">";
+                }
+            }
+
+            MessageBox.Show(text);
+           
+
+            /*
+            foreach(var item in reader)
+            {
+                MessageBox.Show(item);
+            }
+             */
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(Index_By_Name("ccc").ToString());
+
+        }
+
+    }
+}
